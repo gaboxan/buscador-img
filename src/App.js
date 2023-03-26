4f761d829b23618e676dc8b5a16237822c3a12ba
gaboxan <82128741+gaboxan@users.noreply.github.com>
2023-03-26 12:22:06 -0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from "formik";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import './header.css'
+import './content.css'
+import './article.css'
+
+const App = () =>{
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  return(
+    <div>
+      <header>
+        <Formik initialValues={{search: ''}}
+          onSubmit={async values =>{
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers:{
+                'Authorization': 'Client-ID gsjJkPC3HZ34dlzKJjkN3_wlpFY2mKE4YpxAuuGO-nU'
+              }
+            });
+            
+            const data = await response.json();
+            setPhotos(data.results)
+          }}>
+            <Form>
+              <Field name="search">
+
+              </Field>
+            </Form>
+        </Formik>
       </header>
+      <div className="containeer">
+        <div className="center">
+          {photos.map( photo => 
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img  src={[photo.urls.regular, photo.alt_description].join(' - ')}></img>
+              <p>{photo.description}</p>
+            </article>)}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
